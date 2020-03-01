20bfb80515efe9e6feabcca01782c1e0e795883c
Alex Sankov <sankovlex@gmail.com>
2020-03-02 00:28:08 +0400

    ISSUE-1 Add repository implementation based on Entity Framework 6.4

---
+++ src/Waybit.Abstractions.Repository.EntityFramework.IntegrationTests/Fakes/FakeAggregateRootEntityTypeConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.Entity.ModelConfiguration;
+
+namespace Waybit.Abstractions.Repository.EntityFramework.IntegrationTests.Fakes
+{
+	/// <summary>
+	/// Entity framework mapping
+	/// </summary>
+	public class FakeAggregateRootEntityTypeConfiguration
+		: EntityTypeConfiguration<FakeAggregateRoot>
+	{
+		/// <inheritdoc />
+		public FakeAggregateRootEntityTypeConfiguration()
+		{
+			this.ToTable("Fakes");
+
+			this.HasKey(e => e.Id, primaryKey => primaryKey.HasName("Id"));
+			
+			this.Property(e => e.Name)
+				.HasColumnName("Name")
+				.IsRequired();
+		}
+	}
+}
---
+++ src/Waybit.Abstractions.Repository.EntityFramework.IntegrationTests/Fakes/FakeDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System.Data.Common;
+using System.Data.Entity;
+
+namespace Waybit.Abstractions.Repository.EntityFramework.IntegrationTests.Fakes
+{
+	/// <summary>
+	/// Testing entity framework database context
+	/// </summary>
+	public class FakeDbContext : DbContext
+	{
+		/// <inheritdoc />
+		public FakeDbContext(DbConnection existingConnection)
+			: base(existingConnection, false)
+		{
+		}
+
+		/// <summary>
+		/// Testing database set
+		/// </summary>
+		public DbSet<FakeAggregateRoot> FakeAggregateRoots { get; set; }
+
+		/// <inheritdoc />
+		protected override void OnModelCreating(DbModelBuilder modelBuilder)
+		{
+			modelBuilder.Configurations.Add(new FakeAggregateRootEntityTypeConfiguration());
+		}
+	}
+}
